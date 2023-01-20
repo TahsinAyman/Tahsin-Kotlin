59a68dacc6bcb63977516624dfd9a216684c45c3
Tahsin Ayman <87863787+TahsinAyman@users.noreply.github.com>
2023-01-20 07:42:57 +0600

    initial commit

---
+++ DataType.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val string: String = """
+        My Name Is Tahsin
+        What is yours?
+    """;
+    val ch: Char = 'T';
+    val i: Int = 2;
+    val f: Float = 2.5F;
+    val d: Double = 2.10;
+    val lst = arrayOf(1, 2, 3, 4);
+    println(string);
+    println(ch);
+    println(i);
+    println(f);
+    println(d);
+    println(lst);
+    var l: List<String> = ArrayList<String>();
+    print(l);
+
+}
