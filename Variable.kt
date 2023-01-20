64a63d74d476c7460c3a978587b9b7d67551bf3f
Tahsin Ayman <87863787+TahsinAyman@users.noreply.github.com>
2023-01-20 07:32:21 +0600

    initial commit

---
+++ Variable.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val language: String = "Tahsin";
+    // language = "English"; Error
+    var salary: Int;
+    salary = 2;
+    print(String.format("String: %s, Integer: %d", language, salary))
+}
